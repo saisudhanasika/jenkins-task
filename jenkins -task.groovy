7401b43e2d3aeaf370d13e624ad801404e5d7a0d
saisudhanasika <168336256+saisudhanasika@users.noreply.github.com>
2024-05-28 19:38:31 +1000

    initial commit

--- jenkins -task.groovy
+++ jenkins -task.groovy
@@ -3,57 +3,50 @@ pipeline {
     stages {
         stage('Build') {
             steps {
-                echo 'Building...'
-                // Example build tool: Maven
-                // sh 'mvn clean package'
+                echo 'Building.'
+                
             }
         }
         stage('Unit and Integration Tests') {
             steps {
-                echo 'Running Unit and Integration Tests...'
-                // Example test tool: JUnit
-                // sh 'mvn test'
+                echo 'Running Unit and Integration Tests.'
+               
             }
         }
         stage('Code Analysis') {
             steps {
                 echo 'Running Code Analysis...'
-                // Example code analysis tool: SonarQube
-                // sh 'sonar-scanner'
+               
             }
         }
         stage('Security Scan') {
             steps {
-                echo 'Running Security Scan...'
-                // Example security scan tool: OWASP Dependency Check
-                // sh 'dependency-check.sh --project myApp --scan .'
+                echo 'Running Security Scan.'
+                
             }
         }
         stage('Deploy to Staging') {
             steps {
-                echo 'Deploying to Staging...'
-                // Example deployment: AWS CLI
-                // sh 'aws deploy ...'
+                echo 'Deploying to Staging.'
+                
             }
         }
         stage('Integration Tests on Staging') {
             steps {
-                echo 'Running Integration Tests on Staging...'
-                // Example test tool: Postman/Newman
-                // sh 'newman run tests.postman_collection.json'
+                echo 'Running Integration Tests on Staging.'
+                
             }
         }
         stage('Deploy to Production') {
             steps {
-                echo 'Deploying to Production...'
-                // Example deployment: AWS CLI
-                // sh 'aws deploy ...'
+                echo 'Deploying to Production.'
+                
             }
         }
     }
     post {
         always {
-            echo 'Sending Email Notification...'
+            echo 'Sending Email Notification.'
             emailext (
                 to: 'saisudhanasika5@gmail.com',
                 subject: "Jenkins Build ${currentBuild.currentResult}: ${env.JOB_NAME} ${env.BUILD_NUMBER}",
